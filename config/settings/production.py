e08044ee80b5c5b49ea3f25dce4e891f1c02ca01
NnannaMari09033 <nnannamari@gmail.com>
2026-04-09 02:10:53 -0400

    fifth crash fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,16 +1,13 @@
 from .base import *
+import os
 
 DEBUG = False
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[])
-
-# Whitenoise for static files
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Security settings
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True

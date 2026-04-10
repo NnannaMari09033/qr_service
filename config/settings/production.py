8365cc1a7fcf297668cbe07e239429f74d1b9ba4
NnannaMari09033 <nnannamari@gmail.com>
2026-04-10 11:26:33 -0400

    added logs to the production settings to  track errors

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,3 +11,25 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}

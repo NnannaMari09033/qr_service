7816a06746a2473f6a5ca8e21421fa9332e57302
NnannaMari09033 <nnannamari@gmail.com>
2026-04-29 07:51:35 -0400

    Add wsgi-level diagnostics for ALLOWED_HOSTS

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
+print(f"[wsgi] DJANGO_SETTINGS_MODULE = {os.environ.get('DJANGO_SETTINGS_MODULE', '<not set>')}", file=sys.stderr, flush=True)
+print(f"[wsgi] DJANGO_ALLOWED_HOSTS env = {os.environ.get('DJANGO_ALLOWED_HOSTS', '<not set>')!r}", file=sys.stderr, flush=True)
+print(f"[wsgi] DJANGO_DEBUG env = {os.environ.get('DJANGO_DEBUG', '<not set>')!r}", file=sys.stderr, flush=True)
+print(f"[wsgi] DJANGO_SECRET_KEY set? {bool(os.environ.get('DJANGO_SECRET_KEY'))}", file=sys.stderr, flush=True)
+
 application = get_wsgi_application()
+
+from django.conf import settings as _s
+print(f"[wsgi] settings.ALLOWED_HOSTS = {_s.ALLOWED_HOSTS!r}", file=sys.stderr, flush=True)
+print(f"[wsgi] settings.DEBUG = {_s.DEBUG!r}", file=sys.stderr, flush=True)
